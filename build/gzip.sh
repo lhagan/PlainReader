bd7dded7e2dc9db75519e85338a942c994a8e9f1
lhagan <luke@lukehagan.com>
2012-03-25 13:52:55 -0700

    add gzip shell script to ant build script

--- build/gzip.sh
+++ build/gzip.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-pushd ../publish
-find . -name "*.js" -o -name "*.css" -o -name "*.html" -o -name "*.otf"| while read FILE; do echo "gzipping $FILE"; touch $FILE; gzip -9 -c $FILE >$FILE.gz; done
-popd
+find ./publish -name "*.js" -o -name "*.css" -o -name "*.html" -o -name "*.otf"| while read FILE; do echo "gzipping $FILE"; touch $FILE; gzip -9 -c $FILE >$FILE.gz; done
