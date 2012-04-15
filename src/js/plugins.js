4cf2ad459f77b79fe03c7b5ba205adfcacd31cc3
lhagan <luke@lukehagan.com>
2012-04-14 19:18:26 -0700

    clean out old code from plugins.js, split boilerplate functions into their own file

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -3,35 +3,16 @@ part of PlainReader by Luke Hagan
 created: 2011-11-05
 released under the MIT license (see LICENSE.md for details) */
 
-/*global log */
+/*global $, console, window */
+/*jslint white:	true*/
+
 // usage: log('inside coolFunc', this, arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function(){
-  log.history = log.history || [];   // store logs to an array for reference
-  log.history.push(arguments);
-  if(this.console) {
-    arguments.callee = arguments.callee.caller;
-    var newarr = [].slice.call(arguments);
-    (typeof console.log === 'object' ? log.apply.call(console.log, console, newarr) : console.log.apply(console, newarr));
-  }
-};
+window.log = function f(){ log.history = log.history || []; log.history.push(arguments); if(this.console) { var args = arguments, newarr; try { args.callee = f.caller; } catch(e) {} newarr = [].slice.call(args); if (typeof console.log === 'object') { log.apply.call(console.log, console, newarr); } else { console.log.apply(console, newarr);}}};
 
 // make it safe to use console.log always
-(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,timeStamp,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c;}})(function(){try
-{console.log();return window.console;}catch(err){return {};}}());
-
-
-// place any jQuery/helper plugins in here, instead of separate, slower script files.
-/* plugins.css
-part of PlainReader by Luke Hagan
-created: 2011-11-05
-released under the MIT license (see LICENSE.md for details) */
-
-// https://github.com/madrobby/zepto/issues/247
-// with modifications (.each didn't work for some reason)
-$.fn.clone=function() {
-    return this[0].cloneNode(true);
-};
+(function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
+(function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
 
 // strip HTML tags
 // http://stackoverflow.com/questions/822452/strip-html-from-text-javascript
@@ -40,17 +21,6 @@ function stripTags(html) {
    tmp.innerHTML = html;
    return tmp.textContent||tmp.innerText;
 }
-
-// TODO: may be other useful extensions here
-// https://gist.github.com/1379704/f5898782fb366b157c66239e09a96aa52bc9258a
-/*
-  $.fn.scrollTop = function(scrollTop) {
-    this.each(function() {
-      this.scrollTop = scrollTop;
-    });
-    return this;
-  };
-  */
 
 // page scrolling
 // based on: https://github.com/madrobby/zepto/issues/401#issuecomment-4156269
